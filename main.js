a8297e16c5401856b993a7f9781f154e089e1474
wez25 <128694957+wez25@users.noreply.github.com>
2023-03-24 20:07:18 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const main = document.querySelector('.main');
+const con = document.querySelector('.con');
+const txt = document.querySelector('#txt');
+const yes = document.querySelector('#yesBtn');
+const no = document.querySelector('#noBtn');
+
+yes.addEventListener('click', () => {
+    no.style.display = 'none';
+    yes.style.display = 'none';
+    
+    main.style.backgroundColor = '#f27b7b';
+    
+    con.style.border = '2px solid #fff';
+    con.style.backgroundColor = '#f27b7b';
+    con.style.boxShadow = '#d95454 15px 15px 20px inset, #d95454 -15px -15px 20px inset, #cf1e1e 0 0 100px';
+    
+    txt.innerHTML = 'Oppsss wala ng bawian yan ha 🥰<br><br>Labas tayo bukas kahit kain lang namimiss na kasj kita💖';
+    txt.style.color = '#fff';
+    txt.style.textShadow = '1px 1px #f44d4d, -1px -1px #f44d4d, 0 0 20px red';
+});
+
+no.addEventListener('click', () => {
+    let pos1 = Math.floor(Math.random() * 75);
+    let pos2 = Math.floor(Math.random() * -300);
+    no.style.position = 'absolute';
+    no.style.transform = `translate(${pos1}px, ${pos2}px)`;
+});
